990c793c4e3564770f8d6246eadb0841ef8d1f79
Raniya <raniyaak4@gmail.com>
2025-11-16 07:13:33 -0800

    16

--- expense_tracker_app/admin.py
+++ expense_tracker_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+for model_name, model in models.__dict__.items():
+    try:
+        if isinstance(model, type) and issubclass(model, models.models.Model):
+            admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
+
